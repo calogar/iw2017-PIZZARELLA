7a8b5e8cefb8294f8a93b606bb7bf267c1125595
Carlos Isidoro López <carlosisidoro.lopez@gmail.com>
2017-03-13 21:14:48 +0100

    Initial commit.

---
+++ pizzarella/src/main/java/com/calogardev/PizzarellaApplication.java
@@ -0,0 +1,12 @@
+package com.calogardev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzarellaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzarellaApplication.class, args);
+	}
+}
