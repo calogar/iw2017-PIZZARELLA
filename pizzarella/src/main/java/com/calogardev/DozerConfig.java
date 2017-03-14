915c82453ceb179082edaf5b154d66516910ca43
Carlos Isidoro López <carlosisidoro.lopez@gmail.com>
2017-03-14 21:40:47 +0100

    Add Dozer for mapping models and DTOs

---
+++ pizzarella/src/main/java/com/calogardev/DozerConfig.java
@@ -0,0 +1,15 @@
+package com.calogardev;
+
+import org.dozer.DozerBeanMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DozerConfig {
+
+	// Encapsulating the instance so we can change parameters later
+	@Bean
+	public DozerBeanMapper dozer() {
+		return new DozerBeanMapper();
+	}
+}
