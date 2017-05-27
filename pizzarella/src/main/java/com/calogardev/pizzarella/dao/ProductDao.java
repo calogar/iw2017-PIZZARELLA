b6741256850480dd8c6202fa3b3d9638698e86dd
Carlos Isidoro López <carlosisidoro.lopez@gmail.com>
2017-05-27 21:58:50 +0200

    Fix bug with product status

--- pizzarella/src/main/java/com/calogardev/pizzarella/dao/ProductDao.java
+++ pizzarella/src/main/java/com/calogardev/pizzarella/dao/ProductDao.java
@@ -10,10 +10,10 @@ import com.calogardev.pizzarella.model.Product;
 
 public interface ProductDao extends CrudRepository<Product, Long> {
 
-	@Override
-	@Query("SELECT p FROM Product p WHERE p.status = 'ACTIVE'")
-	public List<Product> findAll();
+    @Override
+    @Query("SELECT p FROM Product p WHERE p.status = 'ACTIVE'")
+    public List<Product> findAll();
 
-	@Query("SELECT p FROM Product p WHERE p.id != :id")
-	public List<Product> findAllExceptOne(@Param("id") Long id);
+    @Query("SELECT p FROM Product p WHERE p.id != :id")
+    public List<Product> findAllExceptOne(@Param("id") Long id);
 }
--- pizzarella/src/main/java/com/calogardev/pizzarella/model/Product.java
+++ pizzarella/src/main/java/com/calogardev/pizzarella/model/Product.java
@@ -7,6 +7,8 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -29,6 +31,7 @@ public class Product implements Serializable, Dto {
     private String name;
 
     @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private Status status;
 
     @Column(nullable = false, precision = 3, scale = 2)
